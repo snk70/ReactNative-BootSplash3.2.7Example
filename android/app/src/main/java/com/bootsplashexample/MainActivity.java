a1bc6269d51ff8285de99abcff63d578f72dd94e
Sina <sinakordestani@gmail.com>
2022-11-23 00:30:33 +0330

    Build App and Fix config bugs

--- android/app/src/main/java/com/bootsplashexample/MainActivity.java
+++ android/app/src/main/java/com/bootsplashexample/MainActivity.java
@@ -1,11 +1,19 @@
 package com.bootsplashexample;
 
+import android.os.Bundle; // <- add this necessary import
+
 import com.facebook.react.ReactActivity;
+import com.zoontek.rnbootsplash.RNBootSplash; // <- add this necessary import
+
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
-
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // display the generated bootsplash.xml drawable over our MainActivity
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
